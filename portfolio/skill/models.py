1e30568fca300fd492273f08bc177aaa3827bb71
ksishafiq007 <ksishafiq007@gmail.com>
2020-10-10 18:07:07 +0600

    Database create in django

--- portfolio/skill/models.py
+++ portfolio/skill/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Myskill(models.Model):
+    image=models.ImageField(upload_to='skillimage/')
+    title=models.CharField(max_length=50,blank=False)
+    desc=models.TextField(max_length=500,blank=True)
+    datetime=models.DateTimeField(auto_now_add=False)    
+
+    def __str__(self):
+        return self.title
+                     
+
