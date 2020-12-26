704743dd23623d6f60008f9bffb23d5b348e1efc
ksishafiq007 <ksishafiq007@gmail.com>
2020-12-26 17:59:48 +0600

    data view from database

--- ksi/skill/views.py
+++ ksi/skill/views.py
@@ -1,11 +1,18 @@
 
 from django.shortcuts import render
+from .models import Myskill
 
 def homepage(request):
+    item=Myskill.objects.all()
     title='Welcome to KSI bd'
     desc='I am KSI. I am a software developer'
+    context={
+        'title':title,
+        'description':desc,
+        'data':item
+    }
     
-    return render(request,'index.html',{'title':title,'description':desc})
+    return render(request,'index.html',context)
 
 def aboutpage(request):
     title='About page for skill app'
