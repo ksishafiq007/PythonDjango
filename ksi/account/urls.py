31d904e6f301f032edf9985373736ec5ac2ceb75
ksishafiq007 <ksishafiq007@gmail.com>
2020-12-29 12:57:30 +0600

    Login and logout process

--- ksi/account/urls.py
+++ ksi/account/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import url
-from .views import user_log
+from .views import user_log,user_logout
 
 urlpatterns = [
     url('',user_log, name='login'),
+    url('logout/',user_logout, name='logout'),
 ]
--- ksi/account/views.py
+++ ksi/account/views.py
@@ -1,9 +1,25 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib.auth import authenticate,login,logout
 
 # Create your views here.
 
 def user_log(request):
+    if request.method=='POST':
+        username=request.POST['username']
+        password=request.POST['password']
+        user=authenticate(request,username=username,password=password)
+        if user is not None:
+            login(request,user)
+            return redirect('home')
+
+            #print('username: '+username)
+        else:
+            print('user not found')  
     return render(request,'./auth/login.html',{})
+
+def user_logout(request):
+    logout(request)
+    return redirect('login')
